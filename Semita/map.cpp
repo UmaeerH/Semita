b7b312df7587bfba68921662db32eef3295b9dc5
UmaeerH <umaeerh04@gmail.com>
2025-05-17 10:49:17 +0100

    Full demo map

--- Semita/map.cpp
+++ Semita/map.cpp
@@ -5,10 +5,22 @@
 
 // Map layout
 static const std::unordered_map<std::string, std::vector<std::string>> adjacencyList = {
-    {"Town", {"Woodlands", "Fields", "Pilgrim\'s Path"}},
+    {"Town", {"Woodlands", "Fields", "Pilgrim's path"}},
     {"Woodlands", {"Town", "Deep Woodlands"}},
-    {"Fields", {"Town", "Shore"}},
-    {"Shore", {"Fields"}}
+    {"Fields", {"Town", "Shore", "Mountain path"}},
+    {"Shore", {"Fields", "Fishing village"}},
+    {"Fishing village", {"Shore"}},
+    {"Mountain path", {"Fields", "Mountain shrine"}},
+    {"Mountain shrine", {"Mountain path", "Mountain peak"}},
+    {"Mountain peak", {"Mountain shrine"}},
+    {"Deep Woodlands", {"Cave Entrance", "Woodlands"}},
+    {"Cave Entrance", {"Deep Cave", "Deep Woodlands"}},
+    {"Deep Cave", {"Cave Entrance"}},
+    {"Pilgrim's path", {"Town", "Desert"}},
+    {"Desert", {"Desert Temple", "Pilgrim's path", "Oasis", "Desert village"}},
+    {"Desert Temple", {"Desert"}},
+    {"Oasis", {"Desert", "Desert village"}},
+    {"Desert village", {"Oasis", "Desert"}}
 };
 
 bool checkNeighbours(const std::string& from, const std::string& to) {
